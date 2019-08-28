d5c499ba0ff70759ecfcc67f5a0cede10aaa59b8
stevencxb <chenxb1993@126.com>
2019-08-28 19:28:09 +0800

    增加获取顶层activity和通过顶层activity打开新的activity的方法

--- mvp/src/main/java/com/catchpig/mvp/manager/ActivityManager.kt
+++ mvp/src/main/java/com/catchpig/mvp/manager/ActivityManager.kt
@@ -1,6 +1,7 @@
 package com.catchpig.mvp.manager
 
 import android.app.Activity
+import android.content.Intent
 import java.util.*
 
 object ActivityManager {
@@ -20,10 +21,25 @@ object ActivityManager {
     }
 
     /**
+     * 获取最顶层的activity
+     */
+    fun getTopActivity():Activity{
+        return activities.last()
+    }
+
+    /**
+     * 打开activity
+     */
+    fun startActivity(cls:Class<Activity>){
+        var activity = getTopActivity()
+        var intent = Intent(activity,cls)
+        activity.startActivity(intent)
+    }
+    /**
      * 删除除最上层之外的所有activity
      */
     fun finishAllActivitesExceptTop(){
-        var topActivity = activities[activities.size-1]
+        var topActivity = getTopActivity()
         var iterator = activities.iterator()
         while (iterator.hasNext()){
             val activity = iterator.next()
